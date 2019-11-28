c85ef3a28ad280936b4bacebca8694fb38e00184
Dencio <dencio@macbook-pro.lan>
2019-11-28 19:32:35 +1300

    Updates

---
+++ precache-manifest.52abd8dcff672a4587709ddb70b4e1bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b69c9c556ce889600aac71525066c5b",
+    "url": "/reactjs-redux/index.html"
+  },
+  {
+    "revision": "c04683439687fde7270a",
+    "url": "/reactjs-redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cd266f5cf57589d1eb87",
+    "url": "/reactjs-redux/static/css/main.d5b98071.chunk.css"
+  },
+  {
+    "revision": "c04683439687fde7270a",
+    "url": "/reactjs-redux/static/js/2.2d9288a4.chunk.js"
+  },
+  {
+    "revision": "cd266f5cf57589d1eb87",
+    "url": "/reactjs-redux/static/js/main.daf0112c.chunk.js"
+  },
+  {
+    "revision": "72ab3ad6a50bcad5b718",
+    "url": "/reactjs-redux/static/js/runtime-main.990cdc96.js"
+  },
+  {
+    "revision": "54c761ccedaaa0c52f3833ab18263e52",
+    "url": "/reactjs-redux/static/media/bot.bold.54c761cc.ttf"
+  }
+]);
